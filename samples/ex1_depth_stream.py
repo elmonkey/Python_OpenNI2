5361a635232add6e68fa425b86d5c9d2f8ecad9e
Carlos Torres <carlitos408@gmail.com>
2015-06-21 13:46:56 -0700

    Update ex1_depth_stream.py

--- samples/ex1_depth_stream.py
+++ samples/ex1_depth_stream.py
@@ -10,10 +10,11 @@ Requires the following libraries:
     4. OpenCV 2.4.X
 
 Current features:
-1. Convert primensense oni -> numpy
-2. Stream and display rgb and depth 
-    press esc to exit
-3. Sync and registered depth & rgb streams
+    1. Convert primensense oni -> numpy
+    2. Stream and display depth 
+    3. Keyboard commands    
+        press esc to exit
+        press s to save current screen
 
 NOTE: 
     1. On device streams:  IR and RGB streams do not work together
@@ -55,6 +56,11 @@ depth_stream = dev.create_depth_stream()
 #print 'Get b4 video mode', depth_stream.get_video_mode() # Checks depth video configuration
 depth_stream.set_video_mode(c_api.OniVideoMode(pixelFormat=c_api.OniPixelFormat.ONI_PIXEL_FORMAT_DEPTH_1_MM, resolutionX=320, resolutionY=240, fps=30))
 
+## Check and configure the mirroring -- default is True
+# print 'Mirroring info1', depth_stream.get_mirroring_enabled()
+depth_stream.set_mirroring_enabled(False)
+
+
 ## Start the streams
 depth_stream.start()
 
@@ -78,18 +84,27 @@ def get_depth():
     dmap = np.fromstring(depth_stream.read_frame().get_buffer_as_uint16(),dtype=np.uint16).reshape(240,320)  # Works & It's FAST
     d4d = np.uint8(dmap.astype(float) *255/ 2**12-1) # Correct the range. Depth images are 12bits
     d4d = cv2.cvtColor(d4d,cv2.COLOR_GRAY2RGB)
+    # Shown unknowns in black
+    d4d = 255 - d4d    
     return dmap, d4d
 #get_depth
 
 
 ## main loop
+s=0
 done = False
 while not done:
     key = cv2.waitKey(1)
     ## Read keystrokes
-    if (key&255) == 27:
+    key = cv2.waitKey(1) & 255
+    ## Read keystrokes
+    if key == 27: # terminate
         print "\tESC key detected!"
         done = True
+    elif chr(key) =='s': #screen capture
+        print "\ts key detected. Saving image {}".format(s)
+        cv2.imwrite("depth_"+str(s)+'.png', d4d)
+        #s+=1 # uncomment for multiple captures
     #if
     
     ## Streams    
